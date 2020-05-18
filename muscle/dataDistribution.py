1b0fb21d3947fd1b2e3b0f9a8c1a5934f7b55217
dunland <david.unland@web.de>
2020-05-18 13:14:58 +0200

    recording tests 2020-05-16

--- muscle/dataDistribution.py
+++ muscle/dataDistribution.py
@@ -8,13 +8,19 @@ import numpy
 import pandas as pd
 
 # df = pd.read_csv('drum_logs/snaredrum_log20200516.txt')
-df = pd.read_csv('drum_logs/snaredrum_20200516-02_8bit.txt')
-
-df_sorted = df.sort_values(by='0')
+df = pd.read_csv('drum_logs/hihat_20200516-02_normalized.txt')
+df
+df_sorted = df.sort_values(by='1')
 len(df_sorted)
 
 # %%
 
-fig = plt.figure()
+plt.figure()
+plt.gcf()
+# fig, ax = plt.subplots()
 plt.plot(range(0, len(df_sorted), 1), df)  # unsorted
 plt.plot(range(0, len(df_sorted), 1), df_sorted)  # sorted
+plt.ylabel("normalized to 260")
+plt.title("HiHat Contact Mic at TOP, levelled to 260, 2020-05-16")
+plt.legend(['normalized to 260', 'sorted'])
+plt.show()
