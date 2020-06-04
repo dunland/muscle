8535ee082d580a03577441015dd93766a8db4f4b
dunland <david.unland@web.de>
2020-06-04 20:39:21 +0200

    multifunctional midi setup #1

--- muscle/Code/Arduino/piezoDrums00_customTapTempo/piezoDrums00_customTapTempo.ino
+++ muscle/Code/Arduino/piezoDrums00_customTapTempo/piezoDrums00_customTapTempo.ino
@@ -1,4 +1,7 @@
 #include <elapsedMillis.h>
+#inlcude <MIDI.h>
+
+MIDI_CREATE_DEFAULT_INSTANCE();
 
 /* TODO:
    TapTempo forgetTime
@@ -16,20 +19,23 @@ boolean clockSent = false;
 const int noiseFloor = 512; // TODO: create function to assess noiseFloor upon startup
 const int max_val = 1000;
 const int globalThreshold = 50;
-static const uint8_t pins[] = {A0, A1, A2, A3};
-static const uint8_t leds[] = {LED_BUILTIN, LED_BUILTIN, LED_BUILTIN, LED_BUILTIN};
-static const uint8_t numInputs = 1; // that makes 4 input since number 0 is counted as well
+
+const int PIN_TAP_ACTIVATOR = A3;
+const int PIN_TAP_TRIGGER = A0;
+static const uint8_t pins[] = {A0, A1, A2};
+static const uint8_t leds[] = {LED_BUILTIN, LED_BUILTIN, LED_BUILTIN, LED_BUILTIN}; // array when using SPRESENSE
+static const uint8_t numInputs = 1;
 int vals[numInputs];
 
 void setup() {
 
   Serial.begin(31250); // required clock rate for midi signals
+  MIDI.begin(MIDI_CHANNEL_OMNI); // ATTENTION: do these two work together???
 
   // set all required pins
   for (int i = 0; i < numInputs; i++)
   {
     pinMode(leds[i], OUTPUT);
-    //pinMode(pins[i], INPUT);
   }
 
 }
@@ -38,19 +44,29 @@ void loop() {
 
   //Serial.println(analogRead(A0)); // debugging signal
 
-  // -------------------------------------- read button
-  int val = noiseFloor - analogRead(A0);
-  val = abs(val); // positive values only
-  val = map(val, 0, max_val, 0, 255); // 8-bit conversion
+  /* --------------------------------- TAP TEMPO --------------------------------- */
 
-  // -------------------------------------- tap tempo
-  if (val > globalThreshold && !firstHit)
+  // -------------------------------------- tap activator
+  int val = pinValue(PIN_TAP_ACTIVATOR);
+  boolean waitingForTap = false;
+  
+  if (val > globalThreshold)
+  {
+    waitingForTap = true;
+  }
+
+  // -------------------------------------- tap trigger
+
+  val = pinValue(PIN_TAP_TRIGGER);
+  // hit:
+  if (val > globalThreshold && !firstHit && waitingForTap)
   {
     //digitalWrite(LED_BUILTIN, HIGH);
     firstHitTime = 0;
     firstHit = true;
   }
 
+  // hit:
   if (val > globalThreshold && firstHit && firstHitTime > 50) // 50 ms debounce time
   {
     if (firstHitTime < 2000) // skip tap evaluation when time was too long
@@ -59,6 +75,7 @@ void loop() {
       bpm = 60000 / firstHitTime;
     }
     firstHit = false;
+    waitingforTap = false; // prevent multiple tap detections
   }
 
   // -------------------------------------- blink LED
@@ -71,7 +88,7 @@ void loop() {
     digitalWrite(LED_BUILTIN, LOW);
   }
 
-  // -------------------------------------- MIDI sync
+  // -------------------------------------- MIDI Clock Sync
   /* MIDI Clock events are sent at a rate of 24 pulses per quarter note
      one tap beat equals one quarter note
      only one midi clock signal should be send per 24th quarter note
@@ -86,4 +103,12 @@ void loop() {
     clockSent = false;
   }
 
+}
+
+int pinValue(int pinNumber_in)
+{
+  int pinVal_temp = noiseFloor - analogRead(pinNumber_in);
+  pinVal_temp = abs(pinVal_temp); // positive values only
+  pinVal_temp = map(pinVal_temp, 0, max_val, 0, 255); // 8-bit conversion
+  return val;
 }
