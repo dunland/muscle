4ed3b7103023bc9c18fcaf496736f32540b124ea
dunland <david.unland@web.de>
2022-01-15 20:03:07 +0100

    dd200_timeControl w/ crash trigger + MIDI Clock

--- Code/teensy3.2/src/Songs/dd200_timeControl.cpp
+++ Code/teensy3.2/src/Songs/dd200_timeControl.cpp
@@ -9,7 +9,18 @@ void Score::run_dd200_timeControl(midi::MidiInterface<HardwareSerial> MIDI)
 
     switch (step)
     {
-    case 0:
+    case 0: // just using midi clock
+        if (setup)
+        {
+            setup = false;
+            Drumset::hihat->set_effect(TapTempo);
+        }
+        Hardware::lcd->setCursor(0, 0);
+        Hardware::lcd->print(Globals::current_BPM);
+
+        break;
+
+    case 1: // crash triggers dd200-time-reallocation
         if (setup)
         {
             delay_time = 0;
@@ -17,34 +28,40 @@ void Score::run_dd200_timeControl(midi::MidiInterface<HardwareSerial> MIDI)
             Drumset::hihat->set_effect(TapTempo);
         }
 
-        // detect BPM change:
-        static int previous_tapInterval = 120;
-        if (Globals::tapInterval != previous_tapInterval)
+        if (Drumset::crash1->timing.wasHit)
         {
-            int smallest_mod = 99999;
-            int idx = 0;
-            for (int i = 2; i < 128; i++) // starting at <2 will always result in mod = 0
+
+            // detect BPM change:
+            static int previous_tapInterval = 120;
+            if (Globals::tapInterval != previous_tapInterval)
             {
-                if (Globals::tapInterval % Hardware::dd_200_midi_time_map[i] <= smallest_mod) // smaller → update
+                int smallest_mod = 99999;
+                int idx = 0;
+                for (int i = 0; i < 128; i++) // starting at <2 will always result in mod = 0
                 {
-                    if (random(2) > 0)
+                    int a = (Globals::tapInterval > Hardware::dd_200_midi_interval_map[i]) ? Globals::tapInterval : Hardware::dd_200_midi_interval_map[i];
+                    int b = (Globals::tapInterval > Hardware::dd_200_midi_interval_map[i]) ? Hardware::dd_200_midi_interval_map[i] : Globals::tapInterval;
+                    if (a % b <= smallest_mod) // smaller → update
                     {
-                        smallest_mod = Globals::tapInterval % Hardware::dd_200_midi_time_map[i];
-                        idx = i;
+                        if (random(2) > 0)
+                        {
+                            smallest_mod = a % b;
+                            idx = i;
+                        }
                     }
+                    // else if (Globals::tapInterval % Hardware::dd_200_midi_interval_map[i] == smallest_mod) // equal → any of the two
+                    // {
+                    //     if (random(0, 2) > 0)
+                    //     {
+                    //         smallest_mod = Globals::tapInterval % Hardware::dd_200_midi_interval_map[i];
+                    //         idx = i;
+                    //     }
+                    // }
                 }
-                // else if (Globals::tapInterval % Hardware::dd_200_midi_time_map[i] == smallest_mod) // equal → any of the two
-                // {
-                //     if (random(0, 2) > 0)
-                //     {
-                //         smallest_mod = Globals::tapInterval % Hardware::dd_200_midi_time_map[i];
-                //         idx = i;
-                //     }
-                // }
+                delay_time = Hardware::dd_200_midi_interval_map[idx];
+                Synthesizers::dd200->sendControlChange(dd200_DelayTime, delay_time, MIDI);
+                previous_tapInterval = Globals::tapInterval;
             }
-            delay_time = Hardware::dd_200_midi_time_map[idx];
-            Synthesizers::dd200->sendControlChange(dd200_DelayTime, delay_time, MIDI);
-            previous_tapInterval = Globals::tapInterval;
         }
         Hardware::lcd->setCursor(0, 0);
         Hardware::lcd->print(Globals::current_BPM);
@@ -54,6 +71,11 @@ void Score::run_dd200_timeControl(midi::MidiInterface<HardwareSerial> MIDI)
         Hardware::lcd->print(delay_time);
 
         Serial.println(random(2));
+        break;
+
+    default:
+        Synthesizers::mKorg->sendNoteOff(31, MIDI);
+        proceed_to_next_score();
         break;
     }
 }
